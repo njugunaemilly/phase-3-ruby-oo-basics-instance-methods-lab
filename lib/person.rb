0df557952a56988aae8de96b1a69ff5dcbb810ac
Emilly <emilly.wanjiku@student.moringaschool.com>
2023-02-10 14:02:29 +0300

    Completed assignment

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+    def talk
+        puts "Hello World!"
+    end
+
+tim = Person.new
+tim.talk
+
+    def walk
+        puts"The Person is walking"
+    end
+
+tim = Person.new
+tim.walk
+end
